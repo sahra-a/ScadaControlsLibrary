f386cf91b96b8f8d453280f3bee5b8ec6504eff2
sahra-a <ozsoysahranur@gmail.com>
2026-07-13 11:16:19 +0300

    feat: Add PLC communication initialization

--- ValveControlsLibrary/Communication/PlcClient.cs
+++ ValveControlsLibrary/Communication/PlcClient.cs
@@ -1,13 +1,30 @@
-﻿//using System;
+﻿using System;
 //using System.Collections.Generic;
 //using System.Linq;
 //using System.Text;
 //using System.Threading.Tasks;
 
+
+
 namespace ScadaControlsLibrary.Communication
 {
     public static class PlcClient
     {
+        #region Fields
 
+        private static string _amsNetId = "";
+        private static int _port = 851;
+
+        #endregion
+
+        #region Initialization
+
+        public static void InitCommunication(string amsNetId, int port)
+        {
+            _amsNetId = amsNetId;
+            _port = port;
+        }
+
+        #endregion
     }
 }
