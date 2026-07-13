9048ac4138c0d897e106ba9aa41a8530ed77f2c7
sahra-a <ozsoysahranur@gmail.com>
2026-07-13 11:51:33 +0300

    feat: Add PLC communication base structure

--- ValveControlsLibrary/Communication/PlcClient.cs
+++ ValveControlsLibrary/Communication/PlcClient.cs
@@ -12,17 +12,55 @@ namespace ScadaControlsLibrary.Communication
     {
         #region Fields
 
-        private static string _amsNetId = "";
+        private static string _amsNetId = string.Empty;
         private static int _port = 851;
+        private static bool _isConnected = false;
 
         #endregion
 
         #region Initialization
 
+        /// <summary>
+        /// PLC haberleşmesi için gerekli bağlantı bilgilerini ayarlar.
+        /// Uygulama başlangıcında bir kez çağrılır.
+        /// </summary>
         public static void InitCommunication(string amsNetId, int port)
         {
             _amsNetId = amsNetId;
             _port = port;
+        }
+
+        #endregion
+
+        #region Connection
+
+        /// <summary>
+        /// PLC bağlantısını kurar.
+        /// (Şimdilik iskelet yapı)
+        /// </summary>
+        public static bool Connect()
+        {
+            _isConnected = true;
+            return _isConnected;
+        }
+
+        /// <summary>
+        /// PLC bağlantısını sonlandırır.
+        /// </summary>
+        public static void Disconnect()
+        {
+            _isConnected = false;
+        }
+
+        /// <summary>
+        /// PLC bağlantı durumunu döndürür.
+        /// </summary>
+        public static bool IsConnected
+        {
+            get
+            {
+                return _isConnected;
+            }
         }
 
         #endregion
